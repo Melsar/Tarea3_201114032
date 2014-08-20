41cae20f05488cd06a5eeee0f01e4400c95f951c
Melsar <m.adansantos.p@gmail.com>
2014-08-20 09:04:59 -0600

    Programa

---
+++ Escritorio/workspace/Tarea3_201114032/src/Tarea3.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Tarea3 {
+	public static void main (String[]arg){;
+	Scanner en=new Scanner (System.in);
+	System.out.println("Ingrese un número para su verificación:");
+	int numero=en.nextInt ();
+	if(numero%2==0){;
+	System.out.println("El número es par");
+	}else{
+		System.out.println("El número es impar");
+	}
+	}
+
+}
